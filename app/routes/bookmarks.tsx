079b26f359e3c3c61b896e0ce8030d1580b6ec80
Adam-Collier <adamcollierart@gmail.com>
2022-05-08 17:58:58 +0100

    tweet styling

--- app/routes/bookmarks.tsx
+++ app/routes/bookmarks.tsx
@@ -1,33 +1,35 @@
 import { json, LoaderFunction, redirect } from '@remix-run/node';
 import { Form, useLoaderData } from '@remix-run/react';
 import { getUser } from '~/utils/session.server';
-import { useMatchesData } from '~/utils/utils';
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const user = await getUser(request);
-
-  if (!user) {
-    return redirect('/');
-  }
-
-  const { id, access_token } = user;
-
-  const bookmarksUrl = new URL(
-    `https://api.twitter.com/2/users/${id}/bookmarks`
-  );
-
-  bookmarksUrl.searchParams.append(
-    'tweet.fields',
-    'context_annotations,created_at'
-  );
-  bookmarksUrl.searchParams.append(
-    'expansions',
-    'author_id,attachments.media_keys'
-  );
-  bookmarksUrl.searchParams.append('user.fields', 'verified,profile_image_url');
-  bookmarksUrl.searchParams.append('media.fields', 'type,url');
-
   try {
+    const user = await getUser(request);
+
+    if (!user) {
+      return redirect('/');
+    }
+
+    const { id, access_token } = user;
+
+    const bookmarksUrl = new URL(
+      `https://api.twitter.com/2/users/${id}/bookmarks`
+    );
+
+    bookmarksUrl.searchParams.append(
+      'tweet.fields',
+      'context_annotations,created_at'
+    );
+    bookmarksUrl.searchParams.append(
+      'expansions',
+      'author_id,attachments.media_keys'
+    );
+    bookmarksUrl.searchParams.append(
+      'user.fields',
+      'verified,profile_image_url'
+    );
+    bookmarksUrl.searchParams.append('media.fields', 'type,url,width,height');
+
     const response = await fetch(bookmarksUrl.toString(), {
       headers: {
         Accept: 'application/json',
@@ -56,7 +58,7 @@ const mediaLookup = (mediaId: string, mediaList: any) =>
 const Bookmarks = () => {
   const data = useLoaderData();
   const { bookmarks } = data;
-  console.log(data);
+  // console.log(data);
 
   if (!bookmarks) {
     return <p>we couldnt grab your bookmarks!</p>;
@@ -75,7 +77,7 @@ const Bookmarks = () => {
         </button>
       </Form>
 
-      <div>
+      <div className="max-w-md w-full mx-auto space-y-8">
         {bookmarks.data.map((tweet, index) => {
           const user = userLookup(tweet.author_id, bookmarks.includes.users);
 
@@ -83,40 +85,69 @@ const Bookmarks = () => {
             tweet?.attachments?.media_keys[0],
             bookmarks.includes.media
           );
-          console.log(media, media?.type, 'this is the media');
 
           return (
             <a
+              className="block flex space-x-4"
               key={index}
               href={`https://twitter.com/${user.username}/status/${tweet.id}`}
             >
-              <header>
+              <div className="relative w-8 h-8 rounded-full bg-gray-100 overflow-hidden shrink-0">
                 <img
+                  className="absolute top-0 left-0 w-full h-full"
                   src={user.profile_image_url}
                   alt={`${user.username} profile`}
                 />
-                <h2>
-                  {user.name} {user.verified ? 'true' : 'false'}
-                </h2>
-              </header>
-              {media && media.type === 'photo' && (
-                <img src={media.url} alt="tweet media" />
-              )}
+              </div>
+              <div className="flex-col space-y-2">
+                <header className="flex space-x-4 items-center">
+                  <div className="flex items-center space-x-1">
+                    <h2>{user.name} </h2>
+                    {user.verified && (
+                      <img
+                        className="w-4 h-4 text-red"
+                        src="/verified.svg"
+                        alt=""
+                      />
+                    )}
+                    <p>
+                      @{user.username} ·{' '}
+                      {/* {new Date(tweet.created_at).toLocaleString()} */}
+                      {new Date(tweet.created_at).toLocaleDateString(
+                        undefined,
+                        {
+                          year: undefined,
+                          month: 'long',
+                          day: 'numeric',
+                        }
+                      )}
+                    </p>
+                  </div>
+                </header>
+                {media && media.type === 'photo' && (
+                  <div
+                    className="relative rounded-md overflow-hidden"
+                    style={{
+                      paddingTop: `${(media.height / media.width) * 100}%`,
+                    }}
+                  >
+                    <img
+                      className="absolute top-0 left-0 w-full h-full"
+                      src={media.url}
+                      alt="tweet media"
+                    />
+                  </div>
+                )}
 
-              {media && media.type === 'video' && (
-                <div>
-                  <p>
-                    This tweet contains a video but Twitters Bookmark API doesnt
-                    currently support grabbing their URL.
-                  </p>
-                </div>
-              )}
-
-              <h2>@{user.username}</h2>
-              <p>{tweet.text}</p>
-
-              <div>
-                <span>{new Date(tweet.created_at).toLocaleString()}</span>
+                {media && media.type === 'video' && (
+                  <div>
+                    <p className="text-xs p-2 bg-blue-100 text-blue-600 rounded-sm">
+                      This tweet contains a video but Twitters Bookmark API
+                      doesnt currently support grabbing their URL.
+                    </p>
+                  </div>
+                )}
+                <p>{tweet.text}</p>
               </div>
             </a>
           );
