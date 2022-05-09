b782260c36672c5595e544dbfb40c79caec30310
Adam Collier <adamcollierart@gmail.com>
2022-05-09 17:30:22 +0100

    button styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,18 @@
-import { json, LoaderFunction } from '@remix-run/node';
-import { Form, Link, useLoaderData } from '@remix-run/react';
-import { getUser } from '~/utils/session.server';
+import type { LoaderFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { Form, Link, useLoaderData } from '@remix-run/react'
+import { getUser } from '~/utils/session.server'
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const user = await getUser(request);
+  const user = await getUser(request)
 
   return json({
     user,
-  });
-};
+  })
+}
 
 export default function Index() {
-  const { user } = useLoaderData();
+  const { user } = useLoaderData()
 
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
@@ -21,11 +22,15 @@ export default function Index() {
         </Link>
       ) : (
         <Form method="post" action="/login">
-          <button name="action" value="login">
+          <button
+            className="rounded-full bg-[#1d9bf0] text-white py-3 px-6 hover:bg-[#1a8cd8]"
+            name="action"
+            value="login"
+          >
             Login
           </button>
         </Form>
       )}
     </main>
-  );
+  )
 }
